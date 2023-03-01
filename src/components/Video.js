8de625b0e816a2fd65ebce611d509157f2273557
Sadid <sadid.jones@gmail.com>
2023-03-01 23:09:19 +0600

    5555

---
+++ src/components/Video.js
@@ -0,0 +1,10 @@
+function Video() {
+  return (
+    <div>
+      <img src="http://placeimg.com/160/90/any" alt="Katherine Johnson" />;
+      <div>React Js Tutorial - components</div>
+    </div>
+  );
+}
+
+export default Video;
